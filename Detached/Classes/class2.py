ccce3022fbc841731aed38d78e257a835358b52d
Dev Vrat <itsdvvs.18@gmail.com>
2019-01-03 12:39:20 +0530

    improved teacher Data Structure by including lecture_slots info

--- Detached/Classes/class2.py
+++ Detached/Classes/class2.py
@@ -1,12 +1,17 @@
+import Detached.Global.Variables.varFile1 as Var
+
+
 # Teacher Data Structure Definition
 class Teacher:
-    def __init__(self, name=None, dept=None, school=None, uid=None, max_lec=0, min_lec=0):
+    def __init__(self, name=None, dept=None, school=None, uid=None, max_lec=0, min_lec=0, current_lec=0):
         self.name = name
         self.department = dept
         self.school = school
         self.uID = uid
         self.maxLectures = max_lec
         self.minLectures = min_lec
+        self.currentLectures = current_lec
+        self.lectureSlots = [[False]*Var.maximumSlots]*Var.totalWorkingDays
         self.semestersTeaching = []
         self.subjects = []
 
@@ -18,10 +23,16 @@ class Teacher:
         print(f"Universal ID: {self.uID}")
         print(f"Maximum no. of Lectures: {self.maxLectures}")
         print(f"Minimum no. of Lectures: {self.minLectures}")
-        print(f"Subjects:")
+        print(f"Current Lectures: {self.currentLectures}")
+        # print("Lecture Slots")        # how to show busy slots?
+        print("Subjects:")
         for subject in self.subjects:
             print(subject)
-        print(f"Teachers:")
+        print("Teachers:")
         for semester in self.semestersTeaching:
             print(semester)
         print()
+
+
+obj = Teacher()
+obj.getinfo()
---
+++ Detached/Global/Variables/varFile1.py
@@ -0,0 +1,3 @@
+# values decided by governing authority
+totalWorkingDays = 5
+maximumSlots = 6        # maximum possibility of total lectures in a day
