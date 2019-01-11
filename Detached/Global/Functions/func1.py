7e3a915b9291cef530659748b69c7a201153e259
Abhishek Trivedi <atrivedi397@outlook.com>
2019-01-11 23:12:29 +0530

    demonstration of creating n-objects

--- Detached/Global/Functions/func1.py
+++ Detached/Global/Functions/func1.py
@@ -4,3 +4,15 @@ def display_2d_list(the_2d_list):
         for each_value in each_list:
             print(each_value)
         print()
+
+
+class class_name:
+    def __init__(self, number=None, color=None):
+        self.data = number
+        self.color = color
+
+
+my_list = [class_name() for i in range(20)]
+
+for item in my_list:
+    print(id(item), type(item))
